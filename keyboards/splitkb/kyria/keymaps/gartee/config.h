95341bd86047502469c527875587aaa0b925ffb5
gartee <dereksgartee@gmail.com>
2022-01-25 00:15:13 -0500

    included OLED keymap and updated OLED config

--- keyboards/splitkb/kyria/keymaps/gartee/config.h
+++ keyboards/splitkb/kyria/keymaps/gartee/config.h
@@ -17,11 +17,11 @@
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_ANIMATIONS
-#    define RGBLIGHT_HUE_STEP  8
-#    define RGBLIGHT_SAT_STEP  8
-#    define RGBLIGHT_VAL_STEP  8
-#    define RGBLIGHT_LIMIT_VAL 150
+    #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves.
+    //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression.
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    //#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_SLEEP
 #endif
 
 // Enable OLED Display.
@@ -34,8 +34,6 @@
 #define MASTER_RIGHT
 
 // Enable Rotary Encoder.
-#define ENCODERS_PAD_A { B12 }
-#define ENCODERS_PAD_B { B13 }
 #define ENCODER_DIRECTION_FLIP
 
 // Lets you roll mod-tap keys
@@ -44,6 +42,7 @@
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 #define SPLIT_USB_DETECT
 #define NO_USB_STARTUP_CHECK
+#define SPLIT_USB_TIMEOUT 1000
 
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 200
